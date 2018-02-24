574908c2cbc8a2dc69a93645993310df989b9004
johneckert <johnteckert@gmail.com>
2018-02-24 12:33:32 -0500

    gallery displays paintings

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -4,16 +4,18 @@ const GalleryList = props => {
   return (
     <ul className="galleryList">
       {" "}
-      {props.paintings.length > 0
-        ? props.paintings.map(painting => {
-            return (
-              <li key={painting.id}>
-                <h4>{painting.title}</h4>
-                <img src={painting.image} alt={painting.name} />
-              </li>
-            );
-          })
-        : null}
+      {props.paintings.length > 0 ? (
+        props.paintings.map(painting => {
+          return (
+            <li key={painting.id}>
+              <h4>{painting.title}</h4>
+              <img src={painting.image} alt={painting.title} />
+            </li>
+          );
+        })
+      ) : (
+        <h3>NO Paintings Found</h3>
+      )}
     </ul>
   );
 };
